2bd87935bad29d36f14fb9ecb3406502b9533bd5
deutschlehrer271-maker <deutschlehrer271@gmail.com>
2025-09-28 17:20:51 -0500

    Add files via upload

---
+++ Lesson 1 - string.py
@@ -0,0 +1,31 @@
+# Lesson 1 data types
+
+# This lesson we look at different types of data and how they are used
+
+dictionary ={"Key":"Value"}
+
+'''
+
+'''
+
+print("Hello World!")  # This is called a string
+
+print(25) #This is an integer
+
+print(12.5123) #This is an this is a float( or decimal numbers)
+
+print(True) #This is a boolean value which has 2 values True or False
+
+print([1,'2',False,3.141]) # This is a list, which can have different types.
+
+print((5,3)) # This is a tuple, it particular type of data
+
+print(dictionary["Key"]) # This is a dictionary type of data
+
+
+print("This is a string") # a list of information treated as a text
+
+print(5 +12) #numerial data as integers can be used for many things, such as counting, indexing where a value is in a set of information 
+
+
+
