b855bf09c1199572fa15d43693f145f127a2794e
deutschlehrer271-maker <deutschlehrer271@gmail.com>
2025-09-28 17:43:54 -0500

    Create Lesson 2 - operations.py

---
+++ Lesson 2 - operations.py
@@ -0,0 +1,10 @@
+'''
+Lesson 2 - basic operations
+'''
+
+print(5+5) # addition operation results  >> 10
+print(5-5) # subtraction operation results >> 0
+print(5*5) # multiplication operation result >> 25
+print(5/5) # division operation reults >> 1
+print(5%5) # remainder operation result >> 0
+print(5\\2) # division floor operation result >> 2
